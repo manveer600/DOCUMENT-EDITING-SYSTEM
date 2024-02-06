649e7e24931464592d504486a5158c111393220a
manveer600 <100722147+manveer600@users.noreply.github.com>
2024-02-07 00:32:55 +0530

    adding the document editing systen

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import userRoutes from "./routers/userRoutes.js";
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
+const app = express();
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        optionsSuccessStatus: 204
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', userRoutes);
+export default app;
---
+++ server/index.js
@@ -0,0 +1,38 @@
+import { Server } from "socket.io";
+import { getDocument, updateDocument } from "./controllers/document.controller.js";
+import dotenv from 'dotenv';
+import connection from "./database/db.js"; 
+import app from './app.js';
+dotenv.config();
+connection();
+
+
+const io = new Server(process.env.SOCKET_PORT, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+// CODE FOR SOCKET.IO IN BACKEND
+io.on('connection', (socket) => {
+    socket.on('get-document', async (documentId) => {
+      const document = await getDocument(documentId);
+      if(document){
+        socket.join(documentId);
+        socket.emit('load-document', document.data);
+      }
+      socket.on("send-changes", (delta) => {
+        socket.broadcast.to(documentId).emit('receive-changes', delta);
+    })
+      
+    socket.on('save-document', async (data) =>{
+      await updateDocument(documentId,data);
+    } )
+  });
+});
+
+app.listen(process.env.APP_PORT,()=>{
+  console.log(`server is up at ${process.env.APP_PORT}`);
+})
+
